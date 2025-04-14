a1b63b5c23d3812f344824ffabfd81297b505aca
unknown <amin.ekramul012@gmail.com>
2025-04-14 15:55:27 +0600

    Update authMiddleware logic

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,22 +1,30 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
-export function authenticateToken(req, res, next) {
+const appConfig = require("../config/appConfig");
+
+const authenticateToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  console.log("Authorization Header:", authHeader); // Log the header for debugging
   if (!authHeader) {
-    res.status(401).json({ message: "Unauthorized" });
-  } else {
-    const token = authHeader?.split(" ")[1];
-    if (token) {
-      jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-        if (err) {
-          res.status(401).json({ message: "Unauthorized" });
-        } else {
-          req.user = payload;
-          next();
-        }
-      });
-    } else {
-      res.status(401).json({ message: "Unauthorized" });
-    }
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
-}
+
+  const token = authHeader.split(" ")[1]; // Expecting 'Bearer <token>'
+  console.log("Token:", token); // Log the token for debugging
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Invalid token format" });
+  }
+  console.log(appConfig.AUTH.JWT_SECRET);
+  jwt.verify(token, appConfig.AUTH.JWT_SECRET, (err, payload) => {
+    if (err) {
+      console.log("JWT Verification Error:", err.message);
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    req.user = payload;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
