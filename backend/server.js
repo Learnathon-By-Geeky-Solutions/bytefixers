f0d780d13ec239f2afb52abd1c963211db98f06b
unknown <amin.ekramul012@gmail.com>
2025-04-14 16:37:59 +0600

    Modify server.js

--- backend/server.js
+++ backend/server.js
@@ -3,44 +3,31 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const multer = require("multer");
 const userRoutes = require("./routes/userRoutes");
-const projectRoutes = require('./routes/projectRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-const teamRoutes = require('./routes/teamRoutes');
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const sendEmailRoutes = require("./routes/sendEmailRoutes");
+const teamRoutes = require("./routes/teamRoutes");
+const fileRoutes = require("./routes/fileRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const calendarRoutes = require("./routes/calendarRoutes");
 app.use(bodyParser.json());
 const cors = require("cors");
-// Define CORS options
 const corsOptions = {
-  origin: "http://localhost:4000",
-  methods: ["GET", "POST", "PUT"]
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
-const http = require('http');
-const { Server } = require('socket.io');
-const server = http.createServer(app);
-const io = new Server(server, { cors: corsOptions });
-
-// Enable CORS for the API endpoints
 app.use(cors(corsOptions));
-
 connectDB();
 app.use("/api/user", userRoutes);
-app.use('/projects', projectRoutes);
-app.use('/tasks', taskRoutes);
-app.use('/teams', teamRoutes);
-
-// Socket.IO for real-time collaboration
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-
-  socket.on('taskUpdated', ({ projectId, updatedTask }) => {
-      io.emit(`project:${projectId}:update`, updatedTask);  // Broadcast updates to clients
-  });
-
-  socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-  });
-});
-
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/sendEmail", sendEmailRoutes);
+app.use("/teams", teamRoutes);
+app.use("/files", fileRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/calendar", calendarRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
