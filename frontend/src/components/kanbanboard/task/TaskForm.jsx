c85bde45f2ec127de669793081395d8389f615c0
Tamim-saad <2005095@cse.buet.ac.bd>
2025-04-18 15:24:04 +0600

    feat: add TaskCreate and TaskDetails components for task management and details view

--- frontend/src/components/kanbanboard/task/TaskForm.jsx
+++ frontend/src/components/kanbanboard/task/TaskForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { TaskColoumn } from "./TaskColoumn";
 
 export const TaskForm = ({ handleAddTask }) => {
   const [taskData, setTaskData] = useState({
@@ -47,6 +48,7 @@ export const TaskForm = ({ handleAddTask }) => {
           + Add Task
         </button>
       </form>
+      <TaskColoumn />
     </div>
   );
 };
