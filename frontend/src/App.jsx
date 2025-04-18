d0cff5afd7372aa41fc523af4c7ca2246754bee2
Tamim-saad <2005095@cse.buet.ac.bd>
2025-04-18 15:22:30 +0600

    feat: update App component structure and enhance Sidebar with routing and project modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { AppRouter } from "./router";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MembersProvider } from "./context/MembersContext";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { NotificationProvider } from "./context/NotificationContext";
+import { TaskProvider } from "./context/TaskContext";
 function App() {
   return (
-    <div className="app">
-      <AppRouter />
-    </div>
+    // <BrowserRouter>
+    <MembersProvider>
+      {/* <TaskProvider> */}
+      <NotificationProvider>
+        <AppRouter />
+      </NotificationProvider>
+      {/* </TaskProvider> */}
+    </MembersProvider>
+
+    // </BrowserRouter>
   );
 }
 
