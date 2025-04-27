722c5947218cd8ebd86c2f3f69128a379eef10ff
Habiba <habibarafique526@gmail.com>
2025-04-28 02:12:26 +0600

    modified

--- frontend/src/components/Board/navbar/navbar.jsx
+++ frontend/src/components/Board/navbar/navbar.jsx
@@ -231,6 +231,7 @@ const TopNavbar = () => {
                           goToTask(task._id);
                         }
                       }}
+                      role="button"
                       className="p-2 hover:bg-gray-100 cursor-pointer border-b last:border-b-0"
                     >
                       <div className="flex items-start">
